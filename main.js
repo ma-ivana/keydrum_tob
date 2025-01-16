bc715e0388494327d12285845b9f6740dd9e2a5a
Ivana Croxcatto <34950950+ma-ivana@users.noreply.github.com>
2025-01-15 21:54:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -75,16 +75,6 @@ const impresionTeclas = keys.forEach(key => {
 
 keyset[0].innerHTML = acc;
 
-window.addEventListener("keydown", function(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  if(!audio) return;
-  audio.currentTime = 0;
-  audio.play();
-  key.classList.add("playing");
-  
-});
-
 window.addEventListener("keypress", function(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
